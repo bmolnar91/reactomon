d7591f067bb97ddfe9e75afa07b601bbe7e40126
Balint Molnar <balintkilencvenegy@gmail.com>
2020-09-20 17:45:38 +0200

    Make PokemonDetailsPage and PokemonDetails functional components and use hooks

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,10 +1,21 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import capitalize from "../util/stringHelper";
 
-class PokemonDetails extends Component {
-  state = {
+const PokemonDetails = (props) => {
+  // state = {
+  //   id: null,
+  //   name: null,
+  //   baseExperience: null,
+  //   height: null,
+  //   weight: null,
+  //   abilities: [],
+  //   types: [],
+  //   image: {},
+  // };
+
+  const [state, setState] = useState({
     id: null,
     name: null,
     baseExperience: null,
@@ -13,48 +24,61 @@ class PokemonDetails extends Component {
     abilities: [],
     types: [],
     image: {},
-  };
+  });
 
-  componentDidMount() {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${this.props.id}`)
-      .then((res) =>
-        this.setState({
-          id: res.data.id,
-          name: capitalize(res.data.name),
-          baseExperience: res.data.base_experience,
-          height: res.data.height,
-          weight: res.data.weight,
-          abilities: res.data.abilities,
-          types: res.data.types,
-          image: res.data.sprites.front_default,
-        })
-      );
-  }
+  // componentDidMount() {
+  //   axios
+  //     .get(`https://pokeapi.co/api/v2/pokemon/${props.id}`)
+  //     .then((res) =>
+  //       setState({
+  //         id: res.data.id,
+  //         name: capitalize(res.data.name),
+  //         baseExperience: res.data.base_experience,
+  //         height: res.data.height,
+  //         weight: res.data.weight,
+  //         abilities: res.data.abilities,
+  //         types: res.data.types,
+  //         image: res.data.sprites.front_default,
+  //       })
+  //     );
+  // }
 
-  render() {
-    return (
-      <div style={{ textAlign: "left", margin: "15px" }}>
-        <img src={this.state.image} alt="" />
-        <p>Name: {this.state.name}</p>
-        <p>Base experience: {this.state.baseExperience}</p>
-        <p>Height: {this.state.height}</p>
-        <p>Weight: {this.state.weight}</p>
-        <p>
-          Abilities:
-          {this.state.abilities.map((ability) => {
-            return <li key={ability.ability.name}>{ability.ability.name}</li>;
-          })}
-        </p>
-        <p>
-          Types:
-          {this.state.types.map((type) => {
-            return <li key={type.type.name}>{type.type.name}</li>;
-          })}
-        </p>
-      </div>
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${props.id}`).then((res) =>
+      setState({
+        id: res.data.id,
+        name: capitalize(res.data.name),
+        baseExperience: res.data.base_experience,
+        height: res.data.height,
+        weight: res.data.weight,
+        abilities: res.data.abilities,
+        types: res.data.types,
+        image: res.data.sprites.front_default,
+      })
     );
-  }
-}
+  }, []);
+
+  return (
+    <div style={{ textAlign: "left", margin: "15px" }}>
+      <img src={state.image} alt="" />
+      <p>Name: {state.name}</p>
+      <p>Base experience: {state.baseExperience}</p>
+      <p>Height: {state.height}</p>
+      <p>Weight: {state.weight}</p>
+      <p>
+        Abilities:
+        {state.abilities.map((ability) => {
+          return <li key={ability.ability.name}>{ability.ability.name}</li>;
+        })}
+      </p>
+      <p>
+        Types:
+        {state.types.map((type) => {
+          return <li key={type.type.name}>{type.type.name}</li>;
+        })}
+      </p>
+    </div>
+  );
+};
 
 export default PokemonDetails;
--- src/components/pages/PokemonDetailsPage.js
+++ src/components/pages/PokemonDetailsPage.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
 import PokemonDetails from "../PokemonDetails";
 
-class PokemonDetailsPage extends Component {
-  render() {
-    return (
-      <div>
-        <h2 style={{ textAlign: "center" }}>Pokemon Details</h2>
-        <PokemonDetails id={this.props.match.params.id}></PokemonDetails>
-      </div>
-    );
-  }
-}
+const PokemonDetailsPage = (props) => {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Pokemon Details</h2>
+      <PokemonDetails id={props.match.params.id}></PokemonDetails>
+    </div>
+  );
+};
 
 export default PokemonDetailsPage;
