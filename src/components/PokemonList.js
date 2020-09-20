9dbec90c4836e1ffc91a18f0bc63234997fadd3e
Balint Molnar <balintkilencvenegy@gmail.com>
2020-09-20 17:26:30 +0200

    Make PokemonList a functional component

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { PropTypes } from "prop-types";
 
 import Pokemon from "./Pokemon";
 
-class PokemonList extends Component {
-  render() {
-    return this.props.pokemons.map((pokemon) => (
-      <li key={pokemon.name}>
-        <Pokemon pokemon={pokemon} />
-      </li>
-    ));
-  }
-}
+const PokemonList = (props) => {
+  return props.pokemons.map((pokemon) => (
+    <li key={pokemon.name}>
+      <Pokemon pokemon={pokemon} />
+    </li>
+  ));
+};
 
 PokemonList.propTypes = {
   pokemons: PropTypes.array.isRequired,
