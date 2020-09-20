ac68ebc27a8d15854050dd345c757da0cba17bc5
Balint Molnar <balintkilencvenegy@gmail.com>
2020-09-20 17:23:35 +0200

    Make PokemonsPage a functional component and implement hooks

--- src/components/pages/PokemonsPage.js
+++ src/components/pages/PokemonsPage.js
@@ -1,35 +1,40 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import PokemonList from "../PokemonList";
 
-class PokemonsPage extends Component {
-  state = {
+const PokemonsPage = (props) => {
+  const [state, setState] = useState({
     next: null,
     previous: null,
     results: [],
-  };
+  });
 
-  componentDidMount() {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) =>
-      this.setState({
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      setIsLoading(true);
+      setState({
+        ...state,
         next: res.data.next,
         previous: res.data.previous,
         results: res.data.results,
-      })
-    );
-  }
+      });
+      setIsLoading(false);
+    });
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <h2 style={{ textAlign: "left", padding: "15px" }}>Pokemons</h2>
-        <ol>
-          <PokemonList pokemons={this.state.results} />
-        </ol>
-      </div>
-    );
-  }
-}
+  let content = (
+    <div>
+      <h2 style={{ textAlign: "left", padding: "15px" }}>Pokemons</h2>
+      <ol>
+        <PokemonList pokemons={state.results} />
+      </ol>
+    </div>
+  );
+
+  return content;
+};
 
 export default PokemonsPage;
