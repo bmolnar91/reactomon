e49ed730dd1c43fccb16cdb9f0337b31143e41cc
Balint Molnar <balintkilencvenegy@gmail.com>
2020-09-20 20:15:15 +0200

    Add first styled component in Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function Navbar() {
   return (
-    <header style={headerStyle}>
+    <Header>
       <Link to="/" style={{ float: "left" }}>
         Home
       </Link>
       <Link to="/pokemons">Pokemons</Link> | <Link to="/types">Types</Link>
-    </header>
+    </Header>
   );
 }
 
-const headerStyle = {
-  backgroundColor: "#eeefff",
-  textAlign: "center",
-  padding: "10px",
-};
+const Header = styled.header`
+  background-color: #eeefff;
+  text-align: center;
+  padding: 15px;
+  &:hover {
+    background-color: #eee6ff;
+  }
+`;
 
 export default Navbar;
